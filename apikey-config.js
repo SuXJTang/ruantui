a8e751398149160c8f9674bd3b99201021aa35e3
SuXJTang <1557003690@qq.com>
2026-05-31 13:43:45 +0800

    填入 DeepSeek API Key

--- apikey-config.js
+++ apikey-config.js
@@ -3,4 +3,4 @@
 // 替换下方为你的 API Key，提交到 git
 // https://platform.deepseek.com/api_keys
 // ============================================
-var DEEPSEEK_API_KEY = '';  // 填你的 sk-xxx
+var DEEPSEEK_API_KEY = 'sk-388dec352ebc4c85bbc666d177c2a249';
